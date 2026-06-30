ad7f4f454d3ab9372ba71f5ed080ade17458c62e
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-06-30 14:24:13 -0500

    Refactor get_location to return coordinates object
    Updated the get_location function to return an object with longitude and latitude instead of raw data. Changed the base URL for the IP location service.

--- frontend/assets/js/models/locationModel.js
+++ frontend/assets/js/models/locationModel.js
@@ -5,15 +5,18 @@ export const localtionDefault = [-74.781, 10.968];
 export async function get_location() {
     try {
         const response = await ip_location.get("/");
-        return response.data;
+        return {
+            lon: response.data.longitude,
+            lat: response.data.latitude
+        }
     } catch (error) {
-        console.error(error);
+        console.error(error.message);
         throw error;
     }
 }
 
 const ip_location = axios.create({
-    baseURL: "http://ip-api.com/json/",
+    baseURL: "http://ipapi.co/json/",
     timeout: 5000,
     headers: { "Content-Type": "application/json" },
 });
