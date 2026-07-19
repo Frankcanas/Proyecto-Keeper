ee1a009c9796c052964b5108196effa70dba6995
valncabs <cabasmirandavalen@gmail.com>
2026-07-19 16:46:07 -0500

    arreglo de ruta para el endpoint de evidence

--- frontend/src/services/endpoints/evidence.js
+++ frontend/src/services/endpoints/evidence.js
@@ -1,9 +1,3 @@
 import {fastApi} from "../../api/fastApi";
 
-export const getEvidence = (id) => fastApi.getById('/evidencias', id);
-
-export const getEvidenceById = (id) => fastApi.getById('/evidencias/reporte', id);
-
-export const createEvidence = (evidenceData) => fastApi.postData('/evidencias/', evidenceData);
-
-export const deleteEvidence = (id) => fastApi.deleteData('/evidencias', id);
+export const createEvidence = (formData) => fastApi.uploadFile("/evidencias/",formData);
